eac1b8474b471a1deca7b7880421fd28cddff243
Sumit Ghosh <sumit@sumit-ghosh.com>
2020-07-10 00:44:48 +0530

    Working version 0.1

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 import json
+import logging
 import os
 import string
 from decimal import Decimal
+from hashlib import md5
 
 import boto3
 from flask import Flask, jsonify, request
 
-from get_price import get_price, InvalidURLException
+from get_details import get_details, InvalidURLException
 
+logging.getLogger().setLevel(logging.INFO)
 app = Flask(__name__)
+
 BARGAINS_TABLE = os.environ['BARGAINS_TABLE']
+SUBSCRIPTIONS_TABLE = os.environ['SUBSCRIPTIONS_TABLE']
+PRODUCTS_TABLE = os.environ['PRODUCTS_TABLE']
+QUEUE = os.environ['QUEUE']
 IS_OFFLINE = os.environ.get('IS_OFFLINE')
 
 if IS_OFFLINE:
@@ -18,9 +25,14 @@ if IS_OFFLINE:
         region_name='localhost',
         endpoint_url='http://localhost:8000'
     )
+    queue = None
 else:
     db = boto3.resource('dynamodb')
-table = db.Table(BARGAINS_TABLE)
+    sqs = boto3.resource('sqs')
+    queue = sqs.get_queue_by_name(QueueName=QUEUE)
+bargains_table = db.Table(BARGAINS_TABLE)
+subscriptions_table = db.Table(SUBSCRIPTIONS_TABLE)
+products_table = db.Table(PRODUCTS_TABLE)
 sns = boto3.client('sns')
 
 
@@ -34,13 +46,20 @@ class DecimalEncoder(json.JSONEncoder):
 app.json_encoder = DecimalEncoder
 
 
+def create_topic(email):
+    translator = str.maketrans(string.punctuation, '-' * len(string.punctuation))
+    topic_name = email.translate(translator)
+    topic_arn = sns.create_topic(Name=topic_name)['TopicArn']
+    return topic_arn
+
+
 @app.route('/api/bargains')
-def get_bargain():
+def get_bargains():
     email = request.args.get('email')
     if not email:
         return jsonify({'error': 'Please provide email'}), 400
 
-    response = table.query(
+    response = bargains_table.query(
         IndexName='emailIndex',
         KeyConditionExpression='email = :email',
         ExpressionAttributeValues={':email': email}
@@ -48,11 +67,13 @@ def get_bargain():
     return jsonify(response['Items'])
 
 
-def create_topic(email):
-    translator = str.maketrans(string.punctuation, '-' * len(string.punctuation))
-    topic_name = email.translate(translator)
-    topic_arn = sns.create_topic(Name=topic_name)['TopicArn']
-    return topic_arn
+@app.route('/api/bargains/<string:bargain_id>')
+def get_bargain(bargain_id):
+    response = bargains_table.get_item(Key={'bargainId': bargain_id})
+    item = response.get('Item')
+    if not item:
+        return jsonify({'error': 'Bargain does not exist'}), 404
+    return jsonify(response['Item'])
 
 
 @app.route('/api/bargains', methods=['POST'])
@@ -64,60 +85,157 @@ def create_bargain():
     if not email:
         return jsonify({'error': 'Please provide email'}), 400
     try:
-        product_url, price = get_price(product_url)
+        details = get_details(product_url)
     except InvalidURLException:
         return jsonify({'error': 'Invalid productUrl'}), 400
+    else:
+        product_url = details['url']
+        price = details['price']
+        title = details['title']
+    bargain_id = md5(f'{email}::{product_url}'.encode('UTF-8')).hexdigest()
+
+    sub = subscriptions_table.get_item(Key={'email': email}).get('Item')
+    if not sub or not sub['subscribed']:
+        response = sns.subscribe(TopicArn=create_topic(email), Protocol='email', Endpoint=email,
+                                 ReturnSubscriptionArn=True)
+        sub_arn = response['SubscriptionArn']
+        subscriptions_table.update_item(
+            Key={'email': email},
+            UpdateExpression='set subscribed=:s, subArn=:sa',
+            ExpressionAttributeValues={':s': True, ':sa': sub_arn}
+        )
+
+    products_table.update_item(
+        Key={'productUrl': product_url},
+        UpdateExpression='set productTitle=:title, latestPrice=:price',
+        ExpressionAttributeValues={':title': title, ':price': price}
+    )
 
     try:
-        response = table.put_item(
-            ConditionExpression='productUrl <> :productUrl AND email <> :email',
+        bargains_table.put_item(
+            ConditionExpression='bargainId <> :bargainId',
             Item={
+                'bargainId': bargain_id,
                 'productUrl': product_url,
-                'latestPrice': price,
-                'email': email
+                'email': email,
+                'productTitle': title
             },
             ExpressionAttributeValues={
-                ':productUrl': product_url,
-                ':email': email
+                ':bargainId': bargain_id
             })
     except db.meta.client.exceptions.ConditionalCheckFailedException as e:
         return jsonify({'error': 'Bargain already added'}), 400
 
-    sns.subscribe(TopicArn=create_topic(email), Protocol='email', Endpoint=email)
-
     return jsonify({
         'productUrl': product_url,
-        'latestPrice': price,
-        'email': email
+        'email': email,
+        'productTitle': title,
+        'bargainId': bargain_id
     })
 
 
-def send_notifications(event, context):
-    response = table.scan()
-    items = response['Items']
-    latest_prices = dict()
-    topic_arns = dict()
-    price_updated = set()
+@app.route('/api/bargains/<string:bargain_id>', methods=['DELETE'])
+def delete_bargain(bargain_id):
+    bargains_table.delete_item(Key={'bargainId': bargain_id})
+    return jsonify({'success': 'Bargain deleted'})
 
-    for item in items:
-        latest_price = latest_prices.get(item['productUrl'], get_price(item['productUrl'])[1])
-        topic_arn = topic_arns.get(item['email'], create_topic(item['email']))
-        # if latest_price != item['latestPrice']:
-        if True:
-            sns.publish(Message=f'{item["productUrl"]} now costs {latest_price}', TopicArn=topic_arn)
-            if item['productUrl'] not in price_updated:
-                table.update_item(
-                    Key={
-                        'productUrl': item['productUrl'],
-                        'email': item['email']
-                    },
-                    UpdateExpression='set latestPrice=:p',
-                    ExpressionAttributeValues={':p': latest_price}
-                )
-                price_updated.add(item['productUrl'])
+
+@app.route('/api/subscriptions/<string:email>')
+def get_subscription(email):
+    response = subscriptions_table.get_item(Key={'email': email})
+    item = response.get('Item')
+    if not item:
+        return jsonify({'error': 'Subscription does not exist'}), 404
+    return jsonify(response['Item'])
+
+
+@app.route('/api/subscriptions/<string:email>', methods=['PUT'])
+def update_subscription(email):
+    subscribed = request.json.get('subscribed') or False
+    sub = subscriptions_table.get_item(Key={'email': email}).get('Item')
+    if not sub:
+        response = sns.subscribe(TopicArn=create_topic(email), Protocol='email', Endpoint=email,
+                                 ReturnSubscriptionArn=True)
+        sub_arn = response['SubscriptionArn']
+    else:
+        sub_arn = sub['subArn']
+
+    subscriptions_table.update_item(
+        Key={'email': email},
+        UpdateExpression='set subscribed=:s, subArn=:sa',
+        ExpressionAttributeValues={':s': subscribed, ':sa': sub_arn}
+    )
+
+    return jsonify({
+        'email': email,
+        'subscribed': subscribed,
+        'subArn': sub_arn
+    })
+
+
+@app.route('/api/products')
+def get_product():
+    product_url = request.args.get('productUrl')
+    if not product_url:
+        return jsonify({'error': 'Please provide productUrl'}), 400
+    response = products_table.get_item(Key={'productUrl': product_url})
+    item = response.get('Item')
+    if not item:
+        return jsonify({'error': 'Product does not exist'}), 404
+    return jsonify(response['Item'])
+
+
+def handle_task(event, context):
+    body = event['Records'][0]['body']
+    product = json.loads(body)
+    logging.info(f'Handling {product["productTitle"]}')
+
+    response = bargains_table.query(
+        IndexName='productUrlIndex',
+        KeyConditionExpression='productUrl = :p',
+        ExpressionAttributeValues={':p': product['productUrl']}
+    )
+    bargains = response['Items']
+    emails = [b['email'] for b in bargains if
+              subscriptions_table.get_item(Key={'email': b['email']}).get('Item')['subscribed']]
+
+    if bargains:
+        latest_price = get_details(product['productUrl'])['price']
+        logging.info(f'Latest price is {latest_price}')
+
+        if latest_price != product['latestPrice']:
+            for email in emails:
+                logging.info(f'Sending notification to {email}')
+                sns.publish(Message=f'{product["productTitle"]} now costs {latest_price}', TopicArn=create_topic(email))
+            products_table.update_item(
+                Key={'productUrl': product['productUrl']},
+                UpdateExpression='set latestPrice=:price',
+                ExpressionAttributeValues={':price': latest_price}
+            )
+            message = 'Price changed: Successfully updated price and notified users'
+        else:
+            message = 'Price has not changed'
+    else:
+        message = 'No user to notify'
 
     body = {
-        'message': 'Successfully updated the prices and accordingly notified the clients',
+        'message': message,
+        'input': event
+    }
+    response = {
+        'statusCode': 200,
+        'body': json.dumps(body)
+    }
+    return response
+
+
+def enqueue_tasks(event, context):
+    products = products_table.scan()['Items']
+    for product in products:
+        queue.send_message(MessageBody=json.dumps(product, cls=DecimalEncoder))
+
+    body = {
+        'message': f'Successfully queued {len(products)} tasks',
         'input': event
     }
     response = {
---
+++ get_details.py
@@ -0,0 +1,78 @@
+import json
+import random
+
+import requests
+from lxml import html
+
+user_agents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0'
+]
+
+
+class InvalidURLException(Exception):
+    pass
+
+
+def get_amazon_details(tree):
+    try:
+        title = tree.xpath('//span[@id="productTitle"]/text()')[0].strip()
+        availability = tree.xpath('//div[@id="availability"]/span/text()')[0]
+        if 'Currently unavailable' in availability:
+            return {
+                'price': None,
+                'title': title
+            }
+        price = tree.xpath('//span[@id="priceblock_dealprice"]/text()')
+        if not price:
+            price = tree.xpath('//span[@id="priceblock_ourprice"]/text()')
+        price = price[0].translate(str.maketrans('', '', ',₹')).strip()
+        price = int(float(price))
+    except Exception:
+        raise InvalidURLException('url is not a valid amazon.in product page')
+    return {
+        'price': price,
+        'title': title
+    }
+
+
+def get_flipkart_details(tree):
+    try:
+        price = tree.xpath('//div[@class="_1vC4OE _3qQ9m1"]/text()')[0]
+        price = int(price[1:].replace(',', ''))
+        title = tree.xpath('//span[@class="_35KyD6"]/text()')[0].strip()
+    except Exception:
+        raise InvalidURLException('url is not a valid flipkart.com product page')
+    return {
+        'price': price,
+        'title': title
+    }
+
+
+def get_details(url):
+    try:
+        page = requests.get(url, headers={'User-Agent': random.choice(user_agents)})
+    except Exception:
+        raise InvalidURLException('url is not accessible')
+    try:
+        tree = html.fromstring(page.content)
+        canonical_link = tree.xpath('//link[@rel="canonical"]/@href')[0]
+    except IndexError:
+        raise InvalidURLException('url is not a valid amazon.in or flipkart.com product page')
+    if 'www.amazon.in' in canonical_link:
+        details = get_amazon_details(tree)
+    elif 'www.flipkart.com' in canonical_link:
+        details = get_flipkart_details(tree)
+    else:
+        raise InvalidURLException('url is not a valid amazon.in or flipkart.com product page')
+    return {
+        'price': details['price'],
+        'url': canonical_link,
+        'title': details['title']
+    }
+
+
+if __name__ == '__main__':
+    url = input('[*] Enter URL: ')
+    details = get_details(url)
+    print(json.dumps(details, indent=2))
